c0071758edef4e865113d3a3328d7775807fee49
Marcus Backlund <marcbacklund@gmail.com>
2020-10-13 16:56:59 +0300

    Navigation finished

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../App.css';
+import logo from '../logoplaceholder.png';
  
 import { NavLink } from 'react-router-dom';
- 
+
 const Navigation = () => {
     return (
        <div className="navbar">
+          <NavLink to="/"><img className="logo" src={logo} alt="Logo"/></NavLink>
+          <div className="menuitems">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/pricing">Pricing</NavLink>
+          </div>
        </div>
     );
 }
