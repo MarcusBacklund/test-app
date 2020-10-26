bdfc5339aec0a36c65662a7bf00cf15c0e2cbfb4
Marcus Backlund <marcbacklund@gmail.com>
2020-10-26 10:35:34 +0200

    majorchanges

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="background">
           <Navigation />
             <Switch>
              <Route path="/" component={Home} exact/>
